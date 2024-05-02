74698a0ed10bf823fa574faa2d5d33a05ce1ed35
nomi03 <kentanomi@gmail.com>
2024-05-02 19:26:21 +0900

    5章回答(5.1-5.12)

--- ch05/ex01/index.js
+++ ch05/ex01/index.js
@@ -3,13 +3,13 @@ function Function() {
     const a = 1;
     const b = "Z";
     const c = { obj: 1 };
-    console.log(a, b, c);
+    console.log(a, b, c); // 1 Z { obj: 1 }
   }
   {
     const a = 1;
     const b = "Z";
     const c = { obj: 1 };
-    console.log(a, b, c);
+    console.log(a, b, c); // 1 Z { obj: 1 } 
   }
 }
 
